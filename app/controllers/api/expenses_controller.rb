1a8969aca3cdd2f006908b43e25ec0f8ca886e4a
LJ Lagade <java.eljay@gmail.com>
2025-08-13 14:48:00 +0800

    chore: add filter expenses function

--- app/controllers/api/expenses_controller.rb
+++ app/controllers/api/expenses_controller.rb
@@ -1,7 +1,9 @@
 class Api::ExpensesController < ApplicationController
   def index
-    @expenses = Expense.all.order(date: :desc)
-    render json: @expenses
+    expenses = filtered_expenses.order(date: :desc)
+    total = expenses.sum(:amount)
+
+    render json: { expenses: expenses, total: total }
   end
 
   def create
@@ -20,6 +22,30 @@ class Api::ExpensesController < ApplicationController
   end
 
   private
+
+  def filtered_expenses
+    expenses = Expense.all
+    month = params[:month]
+    year = params[:year]
+
+    if month.present? && year.present?
+      if month == "all" && year == "all"
+        # no filter
+      elsif month == "all"
+        expenses = expenses.where("EXTRACT(YEAR FROM date) = ?", year)
+      elsif year == "all"
+        expenses = expenses.where("EXTRACT(MONTH FROM date) = ?", month)
+      else
+        expenses = expenses.where("EXTRACT(MONTH FROM date) = ? AND EXTRACT(YEAR FROM date) = ?", month, year)
+      end
+    elsif month.present?
+      expenses = expenses.where("EXTRACT(MONTH FROM date) = ?", month) unless month == "all"
+    elsif year.present?
+      expenses = expenses.where("EXTRACT(YEAR FROM date) = ?", year) unless year == "all"
+    end
+
+    expenses
+  end
 
   def expense_params
     params.require(:expense).permit(:name, :amount, :date)
